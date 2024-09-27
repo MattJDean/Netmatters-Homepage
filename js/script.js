2b3796a3ff4db165f562312880c0c9cf311cfcaa
Matt Dean <mattjdean88@gmail.com>
2024-09-27 13:00:51 +0100

    progress 1300 27/09/24

--- js/script.js
+++ js/script.js
@@ -36,3 +36,53 @@ document.addEventListener('DOMContentLoaded', function() {
     slides[currentSlide].classList.add('active'); // Show the first slide initially
     dots[currentSlide].classList.add('active'); // Highlight the first dot initially
   });
+
+
+  // Cookies
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const overlay = document.querySelector('.overlay');
+    const cookiesDialog = document.querySelector('.cookies');
+    const acceptBtn = document.querySelector('.accept__btn');
+    const changeBtn = document.querySelector('.change__btn');
+  
+    // Function to show the cookies dialog
+    function showCookiesDialog() {
+      overlay.style.display = 'block';
+      cookiesDialog.style.display = 'block';
+    }
+  
+    // Function to hide the cookies dialog
+    function hideCookiesDialog() {
+      overlay.style.display = 'none';
+      cookiesDialog.style.display = 'none';
+    }
+
+    // Show the cookies dialog on page load
+    showCookiesDialog();
+  
+    // Function to handle accept button click
+    function handleAccept() {
+      localStorage.setItem('cookiesAccepted', 'true');
+      hideCookiesDialog();
+    }
+
+    // Function to handle change settings button click
+    function handleChangeSettings() {
+      localStorage.setItem('cookiesAccepted', 'false');
+      hideCookiesDialog();
+    }
+
+    // Check localStorage to see if cookies have been accepted
+    if (localStorage.getItem('cookiesAccepted') !== 'true') {
+      showCookiesDialog();
+    }
+
+    // Hide the cookies dialog when the accept button is clicked
+    acceptBtn.addEventListener('click', handleAccept);
+    changeBtn.addEventListener('click', handleChangeSettings);
+  });
+
+  // Sticky Header
+
+ 
