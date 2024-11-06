6dac7e44b44830ca82ccf7357e0555347e540d35
Matt Dean <mattjdean88@gmail.com>
2024-11-06 21:46:24 +0000

    index, header, footer and sidebar converted to .php / contact-us page created / database set up for news section

---
+++ header.php
@@ -0,0 +1,60 @@
+
+<!--Header--> 
+
+<header class="main-header">        
+            <div class="header__top">
+                <div class="container">       
+                    <div class="header__content"> 
+
+                        <!--logo-->
+                        <div class="header__logo">
+                            <img src="assets/img/f-logo.png" alt="Netmatters Logo">
+                        </div>
+
+                        <!--xs Search Bar--> <!--Hidden at 768px-->
+                        <div class="header__xs-search-bar">
+                            <form class="xs-form" method="get" action="#" accept-charset="UTF-8" >
+                                <input id="xs-input" type="text" placeholder="Search...">
+                                    <button id="xs-submit">
+                                        <i class="icon-search"></i>
+                                    </button>
+                            </form>
+                        </div>
+
+                        
+
+                        <!--Support Button   Min 992px-->
+                        <div class="header__support">
+                            <i class="icon-mouse"></i>Support
+                        </div>
+
+                        <!--Contact Button    Min 992px-->
+                        <div class="header__contact">
+                            <i class="icon-paper-plane"></i>Contact
+                        </div>
+
+                            <!--md Search Bar 768px - 992px-->
+                            <div class="header__md-search-bar">
+                            <form class="md-form" method="get" action="#" accept-charset="UTF-8">
+                                <input class="md-input" type="text" placeholder="Search...">
+                                    <button id="md-submit">
+                                        <i class="icon-search"></i>
+                                    </button>
+                            </form>
+                        </div>
+                        
+                        <!--Phone Icon    Max 768px-->
+                        <div class="header__phone">
+                            <i class="icon-phone-in-talk"></i>
+                        </div>
+
+                        <!--Burger Menu-->
+                        <div class="header__hamburger">
+                            <span class="line top"></span>
+                            <span class="line middle"></span>
+                            <span class="line bottom"></span>
+                        </div>
+                    </div>
+                </div>     
+            </div>
+</header> 
---
+++ navbar.php
@@ -0,0 +1,152 @@
+
+<!--Nav Menu--> 
+    <nav class="nav__menu main-header">
+        <div class="container">    
+            <ul>
+                <!--Bespoke Software-->
+                <li id="nav__software" class="nav__item">
+                    <i class="nav__icon icon-apps"></i>
+                        <a href="#"><small>Bespoke</small>Software</a>
+                        <!--Sub Banner-->
+                        <div class="sub__banner"> 
+                            <ul id="sub__software" class="submenu container">
+                                <!--Submenu Title-->
+                                <li class="submenu-title">Our Bespoke Software Services</li>
+                                    <!--Submenu Items-->
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-cogs"></i></span><span class="submenu-item-text">Bespoke CRM</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-briefcase"></i></span><span class="submenu-item-text">Business Automation</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-random"></i></span><span class="submenu-item-text">Software Integrations</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-iphone"></i></span><span class="submenu-item-text">Mobile App Development</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-folder-open"></i></span><span class="submenu-item-text">Bespoke Databases</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-transfer"></i></span><span class="submenu-item-text">Sharepoint Development</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-download-alt"></i></span><span class="submenu-item-text">Operational Systems</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-users"></i></span><span class="submenu-item-text">Business Central Implementation</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-laptop-windows"></i></span><span class="submenu-item-text">Internet of Things (IoT) Software</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-cloud"></i></span><span class="submenu-item-text">Intranet Development</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-cloud-download"></i></span><span class="submenu-item-text">Customer Portal Development</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-globe"></i></span><span class="submenu-item-text">Reporting Hub</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-handshake"></i></span><span class="submenu-item-text">SAP S/4HANA Development</span></a></li>
+                            </ul>
+                        </div>
+                </li>
+
+                <!--IT Support-->
+                <li id="nav__it" class="nav__item">
+                    <i class="nav__icon icon-display"></i>
+                        <a href="#"><small>IT</small>Support</a>
+                        <!--Sub Banner-->
+                        <div class="sub__banner">
+                            <ul id="sub__it" class="submenu container">
+                                <!--Submenu Title-->
+                                <li class="submenu-title">Our IT Support Services</li>
+                                    <!--Submenu Items-->
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-headphones"></i></span><span class="submenu-item-text">Managed IT Support</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-briefcase"></i></span><span class="submenu-item-text">Business IT Support</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-laptop"></i></span><span class="submenu-item-text">Office 365 for Business</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-school"></i></span><span class="submenu-item-text">IT Consultancy</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-cloud"></i></span><span class="submenu-item-text">Cloud Service Provider</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-hdd"></i></span><span class="submenu-item-text">Data Backup & Disaster Recovery</span></a></li>
+                            </ul>
+                        </div>
+                </li>
+                                                
+                <!--Digital Marketing-->
+                <li id="nav__marketing" class="nav__item">
+                    <i class="nav__icon icon-bar-graph"></i>
+                        <a href="#"><small>Digital</small>Marketing</a>
+                        <!--Sub Banner-->        
+                        <div class="sub__banner">
+                            <ul id="sub__marketing" class="submenu container">
+                                <!--Submenu Title-->
+                                <li class="submenu-title">Our Digital Marketing Services</li>
+                                    <!--Submenu Items-->
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-search"></i></span><span class="submenu-item-text">Search Engine Optimisation (SEO)</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-money"></i></span><span class="submenu-item-text">Pay Per Click Advertising (PPC)</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-trending-up"></i></span><span class="submenu-item-text">Conversion Rate Optimisation (CRO)</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-envelope"></i></span><span class="submenu-item-text">Email Marketing</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-users"></i></span><span class="submenu-item-text">Social Media Marketing</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-pencil"></i></span><span class="submenu-item-text">Content Marketing</span></a></li>
+                            </ul>
+                        </div>
+                </li>
+                                                
+                <!--Telecoms Services-->
+                <li id="nav__telecoms" class="nav__item">
+                    <i class="nav__icon icon-phone-in-talk"></i>
+                        <a href="#"><small>Telecoms</small>Services</a>
+                        <!--Sub Banner-->
+                        <div class="sub__banner">
+                            <ul id="sub__telecoms" class="submenu container">
+                                <!--Submenu Title-->
+                                <li class="submenu-title">Our Telecoms Services</li>
+                                    <!--Submenu Items-->
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-ticket"></i></span><span class="submenu-item-text">Business Mobile</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-phone"></i></span><span class="submenu-item-text">Hosted VoIP Provider</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-phone-square"></i></span><span class="submenu-item-text">Business VoIP Systems</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-tachometer"></i></span><span class="submenu-item-text">Business Broadband</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-handshake"></i></span><span class="submenu-item-text">Leased Lines Provider</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-phone-in-talk"></i></span><span class="submenu-item-text">3CX Systems</span></a></li>
+                            </ul>
+                        </div>
+                </li>
+                                                
+                <!--Web Design-->
+                <li id="nav__design" class="nav__item">
+                    <i class="nav__icon icon-code"></i>
+                        <a href="#"><small>Web</small>Design</a>
+                        <!--Sub Banner-->
+                        <div class="sub__banner">
+                            <ul id="sub__design" class="submenu container">
+                                <!--Submenu Title-->
+                                <li class="submenu-title">Our Web Design Services</li>
+                                    <!--Submenu Items-->
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-pencil"></i></span><span class="submenu-item-text">Bespoke Website Design</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-shopping-cart"></i></span><span class="submenu-item-text">eCommerce Website Design</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-desktop"></i></span><span class="submenu-item-text">Pay Monthly Website</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-bullhorn"></i></span><span class="submenu-item-text">Branding & Design</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-iphone"></i></span><span class="submenu-item-text">Mobile App Development</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-cloud"></i></span><span class="submenu-item-text">Web Hosting</span></a></li>
+                            </ul>
+                        </div>
+                </li>
+                                                
+                <!--Cyber Security-->
+                <li id="nav__security" class="nav__item">
+                    <i class="nav__icon icon-security"></i>
+                        <a href="#"><small>Cyber</small>Security</a>
+                        <!--Sub Banner-->
+                        <div class="sub__banner">
+                            <ul id="sub__security" class="submenu container">
+                                <!--Submenu Title-->
+                                <li class="submenu-title">Our Cyber Security Services</li>
+                                    <!--Submenu Items-->
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-clipboard"></i></span><span class="submenu-item-text">Cyber Security Assessment</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-clock"></i></span><span class="submenu-item-text">Cyber Security Management</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-flask"></i></span><span class="submenu-item-text">Cyber Penetration Testing</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-school"></i></span><span class="submenu-item-text">Cyber Essentials Certification</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-security"></i></span><span class="submenu-item-text">PCI Compliance</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-lock"></i></span><span class="submenu-item-text">Hacking Prevention</span></a></li>
+                            </ul>
+                        </div>
+                </li>
+                                                
+                <!--Developer Course-->
+                <li id="nav__training" class="nav__item">
+                    <i class="nav__icon icon-school"></i>
+                        <a href="#"><small>Developer</small>Course</a>
+                        <!--Sub Banner-->
+                        <div class="sub__banner">
+                            <ul id="sub__training" class="submenu container">
+                                <!--Submenu Title-->
+                                <li class="submenu-title">Our Developer Course Services</li>
+                                    <!--Submenu Items-->
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-laptop"></i></span><span class="submenu-item-text">Train For A Career In Tech</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-code"></i></span><span class="submenu-item-text">Skills Bootcamp</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-question-circle"></i></span><span class="submenu-item-text">Scion Scheme Frequently Asked Questions</span></a></li>
+                                    <li class="submenu-item"><a href="#"><span class="submenu-item-icon"><i class="icon-handshake"></i></span><span class="submenu-item-text">Scion Collaborators</span></a></li>
+                            </ul>
+                        </div>
+                </li>           
+            </ul>
+        </div>
+    </nav> 
