d58dac604c83d9e69ddc25ad0a3c00bff2fa59fb
Matt Dean <mattjdean88@gmail.com>
2024-12-12 13:34:18 +0000

    readme and env example

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Netmatters-Homepage
- HTML/CSS Assessment
+<h1>Netmatters Homepage Assessment</h1>
+
+<h3>Contact Form Database Connection</h3>
+<p></p>
+<p>validate-form.php works in sequence with contactForm.js and db-config.php</p>
+<p>db-config.php makes use of .env credentials, accessed with Composer's Autoload.</p>
+<p>validate-form.php, db-config.php, vendor folder containing autoload.php and Composer and .env should be in the utilities directory.</p>
+<p>See .env.example for the format of credentials.</p>
--- utilities/db-config-cpanel.php
+++ utilities/db-config-cpanel.php
@@ -1,39 +1,29 @@
 <?php
 
 // Database config
-function loadEnv($path) {
-    if (!file_exists($path)) {
-        die("Missing .env file.");
-    }
 
-    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($lines as $line) {
-        if (strpos(trim($line), '#') === 0) {
-            continue; 
-        }
-        list($key, $value) = explode('=', $line, 2);
-        $key = trim($key);
-        $value = trim($value);
-        $value = trim($value, '"'); 
-        putenv("$key=$value");
-    }
+require_once __DIR__ . '/vendor/autoload.php'; 
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+
+$dotenv->load();
+   
+// Database configuration
+$host = $_ENV['HOST']; 
+$user = $_ENV['USER'];
+$pass = $_ENV['PASS'];
+$db   = $_ENV['DATABASE'];
+
+// Validate the required environment variables
+if (!$host || !$user || !$pass || !$db) {
+    die("Config error.");
 }
 
-// Load .env
-loadEnv(__DIR__ . '/.env');
-    $host = getenv('HOST');
-    $user = getenv('USER');
-    $pass = getenv('PASS');
-    $db   = getenv('DATABASE');
+// Create a MySQL connection
+$conn = new mysqli($host, $user, $pass, $db);
 
-        // Check valid environment variables
-        if (!$host || !$user || !$pass || !$db) {
-            die("Database config error.");
-        }
-
-    $conn = new mysqli($host, $user, $pass, $db);
-
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        } 
-
+// Check for connection errors
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
